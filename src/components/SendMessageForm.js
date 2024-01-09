cf3db4e4d66378237d9756b2694fcd2867d5e32a
JuanDavidQuesada <jdqe1234@gmail.com>
2024-01-09 17:15:57 -0600

    V1

---
+++ src/components/SendMessageForm.js
@@ -0,0 +1,28 @@
+import { FormControl, InputGroup, Form, Button } from "react-bootstrap";
+import { useState } from "react";
+
+const SendMessageForm = ({ sendMessage }) => {
+  const [message, setMessage] = useState("");
+  return (
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault();
+        sendMessage(message);
+        setMessage("");
+      }}
+    >
+      <InputGroup>
+        <FormControl
+          placeholder="message..."
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+
+        <Button variant="primary" type="submit" disabled={!message}>
+          Send
+        </Button>
+      </InputGroup>
+    </Form>
+  );
+};
+export default SendMessageForm;
